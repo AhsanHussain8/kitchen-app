a8d5793318cf8be550290c8cecf9a187b80baebf
Ahsan Hussain <ahsanhussain@Ahsans-MacBook-Pro-2.local>
2019-07-11 16:44:37 -0500

    frontend made to handle filters changing responsivley

---
+++ kitchen-app-frontend/src/components/FilterSelection.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="filter">
+    {{selectedValues}}
+    <div v-for="key in filterKeys">
+      <label for="component-dropdown"> {{ key }} </label>
+      <dropdown id="component-dropdown" :filterKey="key" :options="allOptions[key]"></dropdown>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import dropdown from './dropdown.vue'
+
+export default {
+  name: 'FilterSelection',
+  components: {
+    dropdown
+  },
+  data () {
+    return {
+      filterKeys : ['action', 'dish', 'station'],
+    }
+  },
+  methods: {
+    ...mapActions([
+      'FIND_FILTERED_DATA' // dispatch when selected array changes 
+      ])
+  },
+  computed: {
+    ...mapGetters({
+      allOptions: 'GET_SINGLE_FILTER_OPTIONS',
+      selectedValues: 'GET_SINGLE_SELECTED_VALUE'
+      }),
+  },
+  watch: {
+    selectedValues: {
+      handler() {
+        console.log(this.selectedValues);
+        this.FIND_FILTERED_DATA();
+      },
+      deep: true,
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- kitchen-app-frontend/src/store.js
+++ kitchen-app-frontend/src/store.js
@@ -11,18 +11,25 @@ export default new Vuex.Store({
   		dish : [],
   		station : []
   	},
-  	actionFilterValue : "any",
-  	dishFilterValue : "any",
-  	stationFilterValue : "any",
+  	selectedValues : {
+	  	action : "any",
+	  	dish: "any",
+	  	station : "any",
+	},
   	filterResults: [], 
   	aggregateDurations : {
   		action : {},
   		dish : {},
   		station : {}
   	},
-
   },
   getters: {
+  	GET_SINGLE_FILTER_OPTIONS ( state ) {
+  		return state.filterValues;
+  	},
+  	GET_SINGLE_SELECTED_VALUE ( state ) {
+  		return state.selectedValues;
+  	},
 
   },
   mutations: {
@@ -32,8 +39,12 @@ export default new Vuex.Store({
   	UPDATE_FILTER_RESULTS ({ state }, results ) {
   		this.state.filterResults = results;
   	},
-  	UPDATE_AGGREGATE_STATS({state}, durations ) {
+  	UPDATE_AGGREGATE_DURATIONS({state }, durations ) {
   		this.state.aggregateDurations = durations;
+  	},
+  	UPDATE_SELECTED_VALUE({state }, payload) {
+  		const key = payload.key;
+  		this.state.selectedValues[key] = payload.newVal;
   	}
 
   },
@@ -51,25 +62,28 @@ export default new Vuex.Store({
   			console.log(error);
   		});
   	},
-  	FIND_FILTERED_DATA ({ commit }) {
+  	FIND_FILTERED_DATA ({ commit, state }) {
+  		const selectedValues = this.state.selectedValues;
   		const request = {
   			method: 'PUT',
   			url: 'http://localhost:5000/filterData',
   			params: { 
-  				action : this.state.actionFilterValue,
-  				dish : this.state.dishFilterValue,
-  				station : this.state.stationFilterValue
+  				action : selectedValues.action,
+  				dish : selectedValues.dish,
+  				station : selectedValues.station
   			}
   		};
 
   		axios(request).then( function(response) {
-  			commit('UPDATE_FILTER_RESULTS', response.data['resultsList'])
-  			commit('UPDATE_AGGREGATE_DURATIONS', response.data['aggregateDurations'])
+  			commit('UPDATE_FILTER_RESULTS', response.data['resultsList']);
+  			commit('UPDATE_AGGREGATE_DURATIONS', response.data['aggregateDurations']);
   		})
   		.catch(function (error) {
   			console.log(error);
   		})
   	},
-  	
+  	FILTER_CHANGED ({commit, dispatch}, payload){
+  		commit('UPDATE_SELECTED_VALUE', payload);
+  	}
   }
 })
